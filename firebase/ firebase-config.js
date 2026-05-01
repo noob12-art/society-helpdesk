52434e25731a505e6746344f4ab0fedb0f5be66d
noob12-art <mannnegi013@gmail.com>
2026-05-02 00:02:54 +0530

    added firebase config

---
+++ firebase/ firebase-config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD-7BcS-OWkKKtlffxFbxLz9mKwqOzcgOg",
+  authDomain: "society-helpdesk-9b874.firebaseapp.com",
+  projectId: "society-helpdesk-9b874",
+  storageBucket: "society-helpdesk-9b874.firebasestorage.app",
+  messagingSenderId: "210358422355",
+  appId: "1:210358422355:web:a452b84974ff9ba341efa6"
+};
+
+export const app = initializeApp(firebaseConfig);
