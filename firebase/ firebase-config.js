7721873c1fb6b14c2663c4a377bd54ca6b41feee
noob12-art <mannnegi013@gmail.com>
2026-05-02 19:58:38 +0530

    update firebase-config

--- firebase/ firebase-config.js
+++ firebase/ firebase-config.js
@@ -8,7 +8,8 @@ const firebaseConfig = {
   projectId: "society-helpdesk-9b874",
   storageBucket: "society-helpdesk-9b874.firebasestorage.app",
   messagingSenderId: "210358422355",
-  appId: "1:210358422355:web:a452b84974ff9ba341efa6"
+  appId: "1:210358422355:web:a452b84974ff9ba341efa6",
+  databaseURL: "https://society-helpdesk-9b874-default-rtdb.asia-southeast1.firebasedatabase.app/"
 };
 
 export const app = initializeApp(firebaseConfig);
